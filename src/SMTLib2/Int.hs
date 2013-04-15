fc7a8eb0cdf8852a94ecea05e36844482a219d81
Iavor S. Diatchki <diatchki@galois.com>
2013-04-15 15:36:39 -0700

    Add the theory of integers.

---
+++ src/SMTLib2/Int.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+module SMTLib2.Int where
+
+import SMTLib2.AST
+
+tInt :: Type
+tInt = TApp (I "Int" []) []
+
+num :: Integral a => a -> Expr
+num a = Lit (LitNum (toInteger a))
+
+nNeg :: Expr -> Expr
+nNeg x = app "-" [x]
+
+nSub :: Expr -> Expr -> Expr
+nSub x y = app "-" [x,y]
+
+nAdd :: Expr -> Expr -> Expr
+nAdd x y = app "+" [x,y]
+
+nMul :: Expr -> Expr -> Expr
+nMul x y = app "*" [x,y]
+
+nDiv :: Expr -> Expr -> Expr
+nDiv x y = app "div" [x,y]
+
+nMod :: Expr -> Expr -> Expr
+nMod x y = app "mod" [x,y]
+
+nAbs :: Expr -> Expr
+nAbs x = app "abs" [x]
+
+nLeq :: Expr -> Expr -> Expr
+nLeq x y = app "<=" [x,y]
+
+nLt :: Expr -> Expr -> Expr
+nLt x y = app "<" [x,y]
+
+nGeq :: Expr -> Expr -> Expr
+nGeq x y = app ">=" [x,y]
+
+nGt :: Expr -> Expr -> Expr
+nGt x y = app ">" [x,y]
